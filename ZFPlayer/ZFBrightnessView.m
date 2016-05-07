6416f9dd143ac36dc635223c5d65f94cd40bfa63
yxz <yangxiaozhi@kloudpeak.com>
2016-05-07 15:35:52 +0800

    feat:更改"亮度"的国际化

--- ZFPlayer/ZFBrightnessView.m
+++ ZFPlayer/ZFBrightnessView.m
@@ -68,7 +68,7 @@
 			title.font = [UIFont systemFontOfSize:16];
 			title.textColor = [UIColor colorWithRed:0.25f green:0.22f blue:0.21f alpha:1.00f];
 			title.textAlignment = NSTextAlignmentCenter;
-			title.text = @"亮度";
+			title.text = NSLocalizedString(@"亮度", nil);
 			[self addSubview:title];
 			title;
 		});
