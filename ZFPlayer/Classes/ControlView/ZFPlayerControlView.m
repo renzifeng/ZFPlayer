8fd9af65aff3ae4a83c9bc7b3a04a270ac7a2f77
dengliwen <dengliwen@dengliwendeiMac.local>
2018-06-28 14:34:24 +0800

    Fix crash while totalMovieDuration is zero

--- ZFPlayer/Classes/ControlView/ZFPlayerControlView.m
+++ ZFPlayer/Classes/ControlView/ZFPlayerControlView.m
@@ -308,6 +308,7 @@ static const CGFloat ZFPlayerControlViewAutoFadeOutTimeInterval = 0.25f;
         if (velocity.x > 0) { style = YES; }
         if (velocity.x < 0) { style = NO; }
         if (velocity.x == 0) { return; }
+        if (totalMovieDuration == 0) { return; }
         [self sliderValueChangingValue:self.sumTime/totalMovieDuration isForward:style];
     } else if (direction == ZFPanDirectionV) {
         if (location == ZFPanLocationLeft) { /// 调节亮度
